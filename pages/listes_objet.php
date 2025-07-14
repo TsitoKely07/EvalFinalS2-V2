530daa9d1f8f2b983176ec47bea12556e077e4e8
TsitoKely07 <tsitoratsifa@gmail.com>
2025-07-14 15:26:37 +0300

    V2 Incomplet

--- pages/listes_objet.php
+++ pages/listes_objet.php
@@ -5,13 +5,9 @@ $bdd=connect_base();
 $categories_result = mysqli_query($bdd, "SELECT id_categorie, nom_categorie FROM categorie_objet");
 $selected_category = isset($_GET['category']) ? intval($_GET['category']) : 0;
 
-$querry= "SELECT o.nom_objet, c.nom_categorie, m.nom as proprietaire, e.date_retour, io.nom_image
-FROM objet o 
-JOIN categorie_objet c ON o.id_categorie = c.id_categorie 
-JOIN membre m ON o.id_membre = m.id_membre 
-LEFT JOIN emprunts e ON o.id_objet = e.id_objet 
-LEFT JOIN image_objet io ON o.id_objet = io.id_objet
-GROUP BY o.id_objet";
+$querry= "SELECT o.nom_objet, c.nom_categorie, m.nom as proprietaire, e.date_retour 
+FROM objet o JOIN categorie_objet c ON o.id_categorie = c.id_categorie 
+JOIN membre m ON o.id_membre = m.id_membre LEFT JOIN emprunts e ON o.id_objet = e.id_objet";
 
 if ($selected_category > 0) {
     $querry .= " WHERE o.id_categorie = $selected_category";
@@ -40,10 +36,9 @@ $resultat = mysqli_query($bdd,$querry);
         </select>
         <noscript><input type="submit" value="Filtrer"></noscript>
     </form>
-<a href="ajout_objet.php">Ajouter</a>
+    <a href="ajout_objet.php">Ajouter</a>
     <table border=1 width=500>
 <tr>
-    <th>Image</th>
     <th>Nom_objet</th>
     <th>nom_categorie</th>
     <th>proprietaire</th>
@@ -51,13 +46,6 @@ $resultat = mysqli_query($bdd,$querry);
 </tr>
     <?php while($row=mysqli_fetch_assoc($resultat)):?>
         <tr>
-            <td>
-                <?php if (!empty($row['nom_image'])): ?>
-                    <img src="../assets/uploads/<?= htmlspecialchars($row['nom_image']) ?>" alt="Image de <?= htmlspecialchars($row['nom_objet']) ?>" style="max-width: 100px; max-height: 100px;">
-                <?php else: ?>
-                    Pas d'image
-                <?php endif; ?>
-            </td>
             <td><?=$row['nom_objet']?></td>
             <td><?=$row['nom_categorie']?></td>
             <td><?=$row['proprietaire']?></td>
